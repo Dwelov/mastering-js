c3c11c078b7151f71eff829f72aca6369049a1dd
Dwelov <sirajmeher984@gmail.com>
2026-03-07 01:49:09 +0500

    just learned the creation of components in react

---
+++ reactbasics/my-react-app/src/Header.tsx
@@ -0,0 +1,16 @@
+function Header() {
+    return (
+        <header>
+            <h1>My React App</h1>
+            <h2>Successfully created the first component  in react super easy</h2>
+            <nav>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
+export default Header;
